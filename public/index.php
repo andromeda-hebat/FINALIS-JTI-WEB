ee5ee351542b890c26c7bea72a8ecb7e768c4016
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-04 11:57:59 +0700

    Membuath Halaman Beranda Sistem Informasi Bebas Tanggungan

--- public/index.php
+++ public/index.php
@@ -1,3 +1,13 @@
-<?php
-
-echo "Hallo Selamat Datang";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIBT-TA</title>
+</head>
+<body>
+    <h1>Sistem Informasi Bebas Tanggungan</h1>
+    <p>Jurusan Teknologi Informasi</p>
+    <p>Politeknik Negeri Malang</p>
+</body>
+</html>
