c8a79e3a322177feea6610158fe2d4c1312c4e40
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-04 12:09:17 +0700

    Menambahkan style css pada beranda

--- public/index.php
+++ public/index.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/assets/css/index.css">
     <title>SIBT-TA</title>
 </head>
 <body>
+    <div class="sistem">
     <h1>Sistem Informasi Bebas Tanggungan</h1>
     <p>Jurusan Teknologi Informasi</p>
     <p>Politeknik Negeri Malang</p>
+    </div>
+
 </body>
 </html>
