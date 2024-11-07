e14d16bf4b25118cc6e92d8c377cda891bc8dc40
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-07 13:26:17 +0700

    membuat komponen navbar

---
+++ public/components/navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">SIBT-TA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse navbar-nav d-flex justify-content-between" id="navbarNav">
+            <div class="d-flex">
+                <a class="nav-item nav-link active" href="#tentang">Tentang</a>
+                <a class="nav-item nav-link" href="#panduan">Panduan</a>
+                <a class="nav-item nav-link" href="pages/kontak.php">Kontak</a>
+            </div>
+            <a type="button" class="btn btn-primary">Masuk</a>
+        </div>
+    </div>
+</nav>
--- public/index.php
+++ public/index.php
@@ -10,24 +10,8 @@
 </head>
 
 <body>
-    <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">SIBT-TA</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse navbar-nav d-flex justify-content-between" id="navbarNav">
-                <div class="d-flex">
-                    <a class="nav-item nav-link active" href="#tentang">Tentang</a>
-                    <a class="nav-item nav-link" href="#panduan">Panduan</a>
-                    <a class="nav-item nav-link" href="#">Kontak</a>
-                </div>
-                <a type="button" class="btn btn-primary">Masuk</a>
-            </div>
-        </div>
-    </nav>
-
+    <?php include __DIR__ . '/components/navbar.php'?>
+    
     <main class="container pt-5 ">
         <section class="d-flex flex-column justify-content-center align-items-center min-vh-100">
             <div>
