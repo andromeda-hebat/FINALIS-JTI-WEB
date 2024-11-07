b82ef853fb256d17269e9c37460feb27364c8daf
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-07 12:17:26 +0700

    membuat struktur dasar halaman beranda

--- public/index.php
+++ public/index.php
@@ -1,17 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/assets/css/index.css">
     <title>SIBT-TA</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    <div class="sistem">
-    <h1>Sistem Informasi Bebas Tanggungan</h1>
-    <p>Jurusan Teknologi Informasi</p>
-    <p>Politeknik Negeri Malang</p>
-    </div>
+    <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SIBT-TA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse navbar-nav d-flex justify-content-between" id="navbarNav">
+                <div class="d-flex">
+                    <a class="nav-item nav-link active" href="#">Tentang</a>
+                    <a class="nav-item nav-link" href="#">Panduan</a>
+                    <a class="nav-item nav-link" href="#">Kontak</a>
+                </div>
+                <a type="button" class="btn btn-primary">Masuk</a>
+            </div>
+        </div>
+    </nav>
+    
+    <main class="container pt-5 d-flex flex-column justify-content-center align-items-center min-vh-100">
+        <div>
+        <h1>Sistem Informasi Bebas Tanggungan</h1>
+        <p>Jurusan Teknologi Informasi</p>
+        <p>Politeknik Negeri Malang</p>
+        </div>
+
+    </main>
 
 </body>
+
 </html>
