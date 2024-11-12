a45d056ae5939ded1f7c5999864aa15eb9fa9f8e
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-12 13:44:35 +0700

    membuat dasbor admin

--- app/views/pages/admin/dashboard.php
+++ app/views/pages/admin/dashboard.php
@@ -1,1 +1,8 @@
-<h1>Dashboard Admin</h1>
+<div class="d-flex">
+    <!-- Sidebar -->
+    <?php include __DIR__ . '/../../components/sidebar_admin.php' ?>
+    <!-- Konten Utama -->
+    <div class="flex-grow-1">
+        <?php include __DIR__ . '/../../components/top_bar.php' ?>
+    </div>
+</div>
--- app/views/pages/student/dashboard.php
+++ app/views/pages/student/dashboard.php
@@ -1,8 +1,1 @@
-<div class="d-flex">
-    <!-- Sidebar -->
-    <?php include __DIR__ . '/../../components/sidebar_admin.php' ?>
-    <!-- Konten Utama -->
-    <div class="flex-grow-1">
-        <?php include __DIR__ . '/../../components/top_bar.php' ?>
-    </div>
-</div>
+<h1>Dashboard Mahasiswa</h1>
