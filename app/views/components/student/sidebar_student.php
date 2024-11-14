ef51196acd9fb04be59ed85785aac53a355b4683
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-14 21:17:35 +0700

    menambahkan halaman formulir tugas akhir

--- app/views/components/student/sidebar_student.php
+++ app/views/components/student/sidebar_student.php
@@ -3,16 +3,15 @@
 
     <!-- Tautan Navigasi -->
     <nav class="nav flex-column w-100">
-        <a href="#" class="nav-link d-flex align-items-center px-3 text-dark">
+        <a href="/dashboard" class="nav-link d-flex align-items-center px-3 text-dark">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-6 h-100">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
             </svg>
-
             Dashboard
         </a>
-        <a href="#" class="nav-link d-flex align-items-center px-3 text-dark">
+        <a href="/final-project-form" class="nav-link d-flex align-items-center px-3 text-dark">
             <img src="icon-status.png" alt="Ikon Status" width="20" height="20" class="me-2">
             Tugas Akhir
         </a>
