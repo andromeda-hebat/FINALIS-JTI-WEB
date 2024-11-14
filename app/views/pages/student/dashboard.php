f70fc3b11c26dd0780e44e8dc6c69ebe4864e3bd
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-14 20:29:59 +0700

    menambahkan halaman dashboard mahasiswa

--- app/views/pages/student/dashboard.php
+++ app/views/pages/student/dashboard.php
@@ -1,1 +1,11 @@
-<h1>Dashboard Mahasiswa</h1>
+<main>
+    <?php include __DIR__ . '/../../components/student/topbar_student.php' ?>
+    <div class="position-relative d-flex">
+        <?php include __DIR__ . '/../../components/student/sidebar_student.php' ?>
+        <div>
+            <h1>Dashboard</h1>
+
+        </div>
+
+    </div>
+</main>
