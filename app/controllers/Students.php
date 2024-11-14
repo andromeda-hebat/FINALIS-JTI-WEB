c62a022e5bfcebc3cc331d2bca05ad96c29edfa3
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-14 21:18:07 +0700

    membuat routing untuk halaman formulir tugas akhir

--- app/controllers/Students.php
+++ app/controllers/Students.php
@@ -3,11 +3,20 @@
 namespace App\Controllers;
 
 require_once __DIR__ . '/../core/Controller.php';
-require_once __DIR__ . '/../models/Student.php';
+
 
 use App\Core\Controller;
 
 
 class Students extends Controller {
 
+    public function __construct() {
+        
+    }
+    public function finalProject(): void {
+        $data['title'] = "Tugas Akhir";
+        $this->view("templates/header",$data);
+        $this->view("pages/student/final_project_form");
+        $this->view("templates/footer");
+    }
 }
