bef1c1c66d0c4802b654a13ec580bf3f334cd063
vanstevanzaky <stevanzaky87@gmail.com>
2024-11-17 22:22:36 +0700

    menambahkan js accordion pada halaman index untuk section panduan

--- app/views/index.php
+++ app/views/index.php
@@ -10,8 +10,11 @@
         </div>
     </section>
     <section id="tentang" class="d-flex flex-column justify-content-center align-items-center text-center min-vh-100">
-        <h1 style="color: #052C65" class="fw-bold mt-4 pt-0 mb-5">Sistem Informasi <br> Bebas Tanggungan Tugas Akhir</h1>
-        <p class="w-50 mb-5">FINALIS JTI adalah sebuah sistem yang mengurusi sistem informasi bebas tanggungan tugas akhir. Sistem ini dirancang agar mampu memudahkan kebutuhan mahasiswa dalam mengurusi administrasi bebas tanggungan tugas akhir.</p>
+        <h1 style="color: #052C65" class="fw-bold mt-4 pt-0 mb-5">Sistem Informasi <br> Bebas Tanggungan Tugas Akhir
+        </h1>
+        <p class="w-50 mb-5">FINALIS JTI adalah sebuah sistem yang mengurusi sistem informasi bebas tanggungan tugas
+            akhir. Sistem ini dirancang agar mampu memudahkan kebutuhan mahasiswa dalam mengurusi administrasi bebas
+            tanggungan tugas akhir.</p>
         <h3 style="color: #052C65;" class="fw-bold mb-5">Tujuan</h3>
         <div class="d-flex justify-content-center">
             <div class="mx-3">
@@ -57,38 +60,103 @@
     </section>
     <section id="panduan" class="d-flex flex-column justify-content-center align-items-center text-center min-vh-100">
         <h2 class="fw-bold mb-5" style="color: #052C65;">Panduan Pengguna</h2>
-        <ol class="w-50 ms-0 ps-0">
+        <ol id="jacord" class="w-50 ms-0 ps-0">
             <li class="card my-3 shadow" style="background-color: #052C65;">
-                <p class="card-body m-0 text-white fw-bold">
+                <h3 class="card-body m-0 text-white fw-bold">
                     Ungggah Tugas Akhir
-                    <svg class="position-absolute end-0 me-3" width="20" height="20" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="position-absolute end-0 me-3 mt-2" width="20" height="20" viewBox="0 0 30 30" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M8.71109 3.08609C8.62379 3.17318 8.55452 3.27663 8.50726 3.39053C8.46 3.50443 8.43567 3.62653 8.43567 3.74984C8.43567 3.87316 8.46 3.99526 8.50726 4.10916C8.55452 4.22306 8.62379 4.32651 8.71109 4.4136L19.2992 14.9998L8.71109 25.5861C8.62393 25.6733 8.55479 25.7767 8.50761 25.8906C8.46044 26.0045 8.43616 26.1266 8.43616 26.2498C8.43616 26.3731 8.46044 26.4952 8.50761 26.6091C8.55479 26.7229 8.62393 26.8264 8.71109 26.9136C8.79826 27.0008 8.90174 27.0699 9.01563 27.1171C9.12951 27.1642 9.25157 27.1885 9.37484 27.1885C9.49811 27.1885 9.62018 27.1642 9.73406 27.1171C9.84795 27.0699 9.95143 27.0008 10.0386 26.9136L21.2886 15.6636C21.3759 15.5765 21.4452 15.4731 21.4924 15.3592C21.5397 15.2453 21.564 15.1232 21.564 14.9998C21.564 14.8765 21.5397 14.7544 21.4924 14.6405C21.4452 14.5266 21.3759 14.4232 21.2886 14.3361L10.0386 3.08609C9.95151 2.99879 9.84805 2.92952 9.73416 2.88226C9.62026 2.835 9.49816 2.81067 9.37484 2.81067C9.25153 2.81067 9.12943 2.835 9.01553 2.88226C8.90163 2.92952 8.79818 2.99879 8.71109 3.08609Z"
                             fill="white" />
                     </svg>
-
-                </p>
+                </h3>
+                <div class="text-white px-3 pb-3 text-start">
+                    <p class="mb-2">Panduan Pengunggahan Tugas Akhir</p>
+                    <ol>
+                        <li>Login: Masukkan username dan password Anda.</li>
+                        <li>Unduh Template Surat: Cari menu "Template Surat", unduh "Surat Pernyataan Publikasi", dan
+                            cetak.</li>
+                        <li>Isi Surat: Lengkapi semua informasi pada surat yang dicetak.</li>
+                        <li>Scan Surat: Scan surat yang sudah diisi menjadi file PDF.</li>
+                        <li>Unggah Berkas:
+                            <ul style="list-style: disc;">
+                                <li>Laporan Tugas Akhir: Format PDF, maksimal 10MB.</li>
+                                <li>Program/Aplikasi: Format ZIP/RAR, maksimal 1GB.</li>
+                                <li>Surat Pernyataan: File scan PDF.</li>
+                            </ul>
+                        </li>
+                        <li>Konfirmasi: Periksa kembali semua berkas, lalu kirim.</li>
+                    </ol>
+                    <p class="mb-2">Tips:</p>
+                    <ul class="mb-0" style="list-style: disc;">
+                        <li>Pastikan format dan ukuran file sesuai.</li>
+                        <li>Jika ada masalah, hubungi admin.</li>
+                    </ul>
+                </div>
             </li>
             <li class="card my-3 shadow" style="background-color: #052C65;">
-                <p class="card-body m-0 text-white fw-bold">
+                <h3 class="card-body m-0 text-white fw-bold">
                     Administrasi Prodi
-                    <svg class="position-absolute end-0 me-3" width="20" height="20" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="position-absolute end-0 me-3 mt-2" width="20" height="20" viewBox="0 0 30 30" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M8.71109 3.08609C8.62379 3.17318 8.55452 3.27663 8.50726 3.39053C8.46 3.50443 8.43567 3.62653 8.43567 3.74984C8.43567 3.87316 8.46 3.99526 8.50726 4.10916C8.55452 4.22306 8.62379 4.32651 8.71109 4.4136L19.2992 14.9998L8.71109 25.5861C8.62393 25.6733 8.55479 25.7767 8.50761 25.8906C8.46044 26.0045 8.43616 26.1266 8.43616 26.2498C8.43616 26.3731 8.46044 26.4952 8.50761 26.6091C8.55479 26.7229 8.62393 26.8264 8.71109 26.9136C8.79826 27.0008 8.90174 27.0699 9.01563 27.1171C9.12951 27.1642 9.25157 27.1885 9.37484 27.1885C9.49811 27.1885 9.62018 27.1642 9.73406 27.1171C9.84795 27.0699 9.95143 27.0008 10.0386 26.9136L21.2886 15.6636C21.3759 15.5765 21.4452 15.4731 21.4924 15.3592C21.5397 15.2453 21.564 15.1232 21.564 14.9998C21.564 14.8765 21.5397 14.7544 21.4924 14.6405C21.4452 14.5266 21.3759 14.4232 21.2886 14.3361L10.0386 3.08609C9.95151 2.99879 9.84805 2.92952 9.73416 2.88226C9.62026 2.835 9.49816 2.81067 9.37484 2.81067C9.25153 2.81067 9.12943 2.835 9.01553 2.88226C8.90163 2.92952 8.79818 2.99879 8.71109 3.08609Z"
                             fill="white" />
                     </svg>
-                </p>
+                </h3>
+                <div class="text-white px-3 pb-3 text-start ">
+                    <p class="mb-2">Panduan Pengunggahan Administrasi</p>
+                    <ol>
+                        <li>Login: Masukkan username dan password Anda.</li>
+                        <li>Unduh Template Surat: Cari menu "Template Surat", unduh surat yang diperlukan, dan cetak.
+                        </li>
+                        <li>Isi Surat: Lengkapi semua informasi pada surat yang dicetak.</li>
+                        <li>Scan Surat: Scan surat yang sudah diisi menjadi file PDF.</li>
+                        <li>Unggah Berkas:
+                            <ul style="list-style: disc;">
+                                <li>Distribusi Laporan Skripsi</li>
+                                <li>Distribusi Laporan Magang</li>
+                                <li>Bebas Kompensasi</li>
+                                <li>Scan TOEIC</li>
+                            </ul>
+                        </li>
+                        <li>Konfirmasi: Periksa kembali semua berkas, lalu kirim.</li>
+                    </ol>
+                    <p class="mb-2">Tips:</p>
+                    <ul class="mb-0" style="list-style: disc;">
+                        <li>Pastikan format dan ukuran file sesuai.</li>
+                        <li>Jika ada masalah, hubungi admin.</li>
+                    </ul>
+                </div>
             </li>
             <li class="card my-3 shadow" style="background-color: #052C65;">
-                <p class="card-body m-0 text-white fw-bold">
+                <h3 class="card-body m-0 text-white fw-bold">
                     Bebas Tanggungan
-                    <svg class="position-absolute end-0 me-3" width="20" height="20" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg class="position-absolute end-0 me-3 mt-2" width="20" height="20" viewBox="0 0 30 30" fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd"
                             d="M8.71109 3.08609C8.62379 3.17318 8.55452 3.27663 8.50726 3.39053C8.46 3.50443 8.43567 3.62653 8.43567 3.74984C8.43567 3.87316 8.46 3.99526 8.50726 4.10916C8.55452 4.22306 8.62379 4.32651 8.71109 4.4136L19.2992 14.9998L8.71109 25.5861C8.62393 25.6733 8.55479 25.7767 8.50761 25.8906C8.46044 26.0045 8.43616 26.1266 8.43616 26.2498C8.43616 26.3731 8.46044 26.4952 8.50761 26.6091C8.55479 26.7229 8.62393 26.8264 8.71109 26.9136C8.79826 27.0008 8.90174 27.0699 9.01563 27.1171C9.12951 27.1642 9.25157 27.1885 9.37484 27.1885C9.49811 27.1885 9.62018 27.1642 9.73406 27.1171C9.84795 27.0699 9.95143 27.0008 10.0386 26.9136L21.2886 15.6636C21.3759 15.5765 21.4452 15.4731 21.4924 15.3592C21.5397 15.2453 21.564 15.1232 21.564 14.9998C21.564 14.8765 21.5397 14.7544 21.4924 14.6405C21.4452 14.5266 21.3759 14.4232 21.2886 14.3361L10.0386 3.08609C9.95151 2.99879 9.84805 2.92952 9.73416 2.88226C9.62026 2.835 9.49816 2.81067 9.37484 2.81067C9.25153 2.81067 9.12943 2.835 9.01553 2.88226C8.90163 2.92952 8.79818 2.99879 8.71109 3.08609Z"
                             fill="white" />
                     </svg>
-                </p>
+                </h3>
+                <div class="text-white px-3 pb-3 text-start">
+                    <p class="mb-2">Syarat Pengajuan:</p>
+                    <p class="mb-2">Sebelum Anda dapat mengajukan surat bebas tanggungan, pastikan Anda telah
+                        menyelesaikan semua
+                        persyaratan berikut:</p>
+                    <ul class="mb-0" style="list-style: disc;">
+                        <li>Mengunggah Tugas Akhir: Semua berkas tugas akhir, termasuk laporan, program/aplikasi, surat
+                            pernyataan, dan dokumen pendukung lainnya, telah diunggah secara lengkap dan benar melalui
+                            sistem.</li>
+                        <li>Verifikasi Tugas Akhir: Tugas akhir Anda telah diverifikasi oleh dosen pembimbing dan pihak
+                            prodi.</li>
+                        <li>Menyelesaikan Administrasi Prodi: Seluruh administrasi di prodi, seperti pengembalian buku
+                            perpustakaan, pengembalian inventaris, dan urusan lainnya, telah diselesaikan.</li>
+                    </ul>
+                </div>
             </li>
         </ol>
+
     </section>
 </main>
