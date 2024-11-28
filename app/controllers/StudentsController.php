2b2831c9bf2af47bf1c8f64d531f478dd522df8e
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-11-28 10:00:31 +0700

    add some changes from branch dev/farrel

--- app/controllers/StudentsController.php
+++ app/controllers/StudentsController.php
@@ -8,12 +8,13 @@ require_once __DIR__ . '/../core/Controller.php';
 use App\Core\Controller;
 
 
-class Students extends Controller {
+class StudentsController extends Controller {
 
     public function tugasAkhir(): void {
         $data['title'] = "Tugas Akhir";
+        $data['active_page'] = "tugas_akhir";
         $this->view("templates/header",$data);
-        $this->view("pages/student/tugas_akhir");
+        $this->view("pages/student/tugas_akhir", $data);
         $this->view("templates/footer");
     }
     public function tugasAkhirTerkirim(): void {
@@ -24,22 +25,34 @@ class Students extends Controller {
     }
 
     
-    public function administrasi(): void {
+    public function administrasiProdi(): void {
         $data['title'] = "Administrasi Prodi";
+        $data['active_page'] = "administrasi_prodi";
         $this->view("templates/header", $data);
-        $this->view("pages/student/administrasi_prodi");
+        $this->view("pages/student/administrasi_prodi", $data);
         $this->view("templates/footer");
     }
     public function riwayatPengajuan(): void {
         $data['title'] = "Riwayat Pengajuan";
+        $data['active_page'] = "riwayat_pengajuan";
         $this->view("templates/header", $data);
-        $this->view("pages/student/riwayat_pengajuan");
+        $this->view("pages/student/riwayat_pengajuan", $data);
         $this->view("templates/footer");
     }
     public function permintaanSurat(): void {
         $data['title'] = "Permintaan Surat";
+        $data['active_page'] = "permintaan_surat";
         $this->view("templates/header", $data);
-        $this->view("pages/student/permintaan_surat");
+        $this->view("pages/student/permintaan_surat", $data);
         $this->view("templates/footer");
+    }
+
+    public function prosesAdministrasiProdi(): void {
+        $report = $_POST['thesis'];
+        $internship = $_POST['internship'];
+        $compensation = $_POST['compensation'];
+        $toeic = $_POST['toeic'];
+
+        echo "Still in process";
     }
 }
