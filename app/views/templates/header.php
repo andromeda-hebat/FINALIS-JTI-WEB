9ac41fdaebc5b2c1060614dd33bca3f1b1289391
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-01 08:41:55 +0700

    add php templating to prepare custom external css file

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,6 +8,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/reddit-sans@5.1.0/index.min.css">
+
+    <!-- Custom CSS link -->
+    <?php if(isset($data['css'])): ?>
+    <?php foreach($data['css'] as $css_link): ?>
+    <link rel="stylesheet" href="<?= $css_link . '.css' ?>">
+    <?php endforeach; ?>
+    <?php endif; ?>
     
     <style>
         * {
