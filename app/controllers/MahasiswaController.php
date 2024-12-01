10c3633a163a91290b7394ab15daa5c31f6d3c00
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-01 17:34:17 +0700

    add dynamic view to display tugas akhir page based on their status

--- app/controllers/MahasiswaController.php
+++ app/controllers/MahasiswaController.php
@@ -3,26 +3,38 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Models\Berkas;
 
 
-class MahasiswaController extends Controller {
+class MahasiswaController extends Controller
+{
 
-    public function tugasAkhir(): void {
+    private Berkas $berkas;
+
+    public function __construct()
+    {
+        $this->berkas = new Berkas();
+    }
+
+    public function tugasAkhir(): void
+    {
         $data['title'] = "Tugas Akhir";
         $data['active_page'] = "tugas_akhir";
         $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+        $data['req_status'] = $this->berkas->checkUserBerkasProdiStatus($_SESSION['user_id']);
+        $this->view("templates/header", $data);
         $this->view("pages/mahasiswa/tugas_akhir", $data);
         $this->view("templates/footer");
     }
 
-    public function processTugasAkhir(): void {
+    public function processTugasAkhir(): void
+    {
         var_dump($_FILES);
         if (isset($_FILES['tugas_akhir']) && isset($_FILES['program_aplikasi']) && isset($_FILES['publikasi_jurnal'])) {
             $upload_dir = [
-                "tugas_akhir"=>__DIR__ . "/../../storage/tugas_akhir/",
-                "program_aplikasi"=>__DIR__ . "/../../storage/program_aplikasi/",
-                "publikasi_jurnal"=>__DIR__ . "/../../storage/publikasi_jurnal/"
+                "tugas_akhir" => __DIR__ . "/../../storage/tugas_akhir/",
+                "program_aplikasi" => __DIR__ . "/../../storage/program_aplikasi/",
+                "publikasi_jurnal" => __DIR__ . "/../../storage/publikasi_jurnal/"
             ];
 
             foreach ($upload_dir as $key => $path) {
@@ -34,7 +46,7 @@ class MahasiswaController extends Controller {
             $is_move_uploaded_file_success = true;
             foreach ($_FILES as $key => $value) {
                 $file_path = $upload_dir[$key] . uniqid() . '--' . basename($value['name']);
-                
+
                 if (!move_uploaded_file($value['tmp_name'], $file_path)) {
                     $is_move_uploaded_file_success = false;
                 }
@@ -50,23 +62,26 @@ class MahasiswaController extends Controller {
         }
     }
 
-    public function tugasAkhirTerkirim(): void {
+    public function tugasAkhirTerkirim(): void
+    {
         $data['title'] = "Tugas Akhir";
         $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+        $this->view("templates/header", $data);
         $this->view("pages/mahasiswa/ta_terkirim");
         $this->view("templates/footer");
     }
 
-    public function tugasAkhirTerverif(): void {
+    public function tugasAkhirTerverif(): void
+    {
         $data["title"] = "Tugas Akhir";
         $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+        $this->view("templates/header", $data);
         $this->view("pages/mahasiswa/ta_terverifikasi");
         $this->view("templates/footer");
     }
-    
-    public function administrasiProdi(): void {
+
+    public function administrasiProdi(): void
+    {
         $data['title'] = "Administrasi Prodi";
         $data['active_page'] = "administrasi_prodi";
         $data['css'] = ["assets/css/sidebar"];
@@ -75,7 +90,8 @@ class MahasiswaController extends Controller {
         $this->view("templates/footer");
     }
 
-    public function riwayatPengajuan(): void {
+    public function riwayatPengajuan(): void
+    {
         $data['title'] = "Riwayat Pengajuan";
         $data['active_page'] = "riwayat_pengajuan";
         $data['css'] = ["assets/css/sidebar"];
@@ -84,7 +100,8 @@ class MahasiswaController extends Controller {
         $this->view("templates/footer");
     }
 
-    public function permintaanSurat(): void {
+    public function permintaanSurat(): void
+    {
         $data['title'] = "Permintaan Surat";
         $data['active_page'] = "permintaan_surat";
         $data['css'] = ["assets/css/sidebar"];
@@ -93,7 +110,8 @@ class MahasiswaController extends Controller {
         $this->view("templates/footer");
     }
 
-    public function prosesAdministrasiProdi(): void {
+    public function prosesAdministrasiProdi(): void
+    {
         $report = $_POST['thesis'];
         $internship = $_POST['internship'];
         $compensation = $_POST['compensation'];
