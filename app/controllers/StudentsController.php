c694e8b90ea66f93eca72d3fde3c40143a2421b5
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-01 08:44:02 +0700

    add custom external css link in admin prodi, home, and students controller

--- app/controllers/StudentsController.php
+++ app/controllers/StudentsController.php
@@ -10,42 +10,50 @@ class StudentsController extends Controller {
     public function tugasAkhir(): void {
         $data['title'] = "Tugas Akhir";
         $data['active_page'] = "tugas_akhir";
+        $data['css'] = ["assets/css/sidebar"];
         $this->view("templates/header",$data);
         $this->view("pages/student/tugas_akhir", $data);
         $this->view("templates/footer");
     }
-  
+
     public function tugasAkhirTerkirim(): void {
         $data['title'] = "Tugas Akhir";
+        $data['css'] = ["assets/css/sidebar"];
         $this->view("templates/header",$data);
         $this->view("pages/student/ta_terkirim");
         $this->view("templates/footer");
     }
-    public function tugasAkhirTerverif(){
+
+    public function tugasAkhirTerverif(): void {
         $data["title"] = "Tugas Akhir";
+        $data['css'] = ["assets/css/sidebar"];
         $this->view("templates/header",$data);
         $this->view("pages/student/ta_terverifikasi");
         $this->view("templates/footer");
-
     }
     
     public function administrasiProdi(): void {
         $data['title'] = "Administrasi Prodi";
         $data['active_page'] = "administrasi_prodi";
+        $data['css'] = ["assets/css/sidebar"];
         $this->view("templates/header", $data);
         $this->view("pages/student/administrasi_prodi", $data);
         $this->view("templates/footer");
     }
+
     public function riwayatPengajuan(): void {
         $data['title'] = "Riwayat Pengajuan";
         $data['active_page'] = "riwayat_pengajuan";
+        $data['css'] = ["assets/css/sidebar"];
         $this->view("templates/header", $data);
         $this->view("pages/student/riwayat_pengajuan", $data);
         $this->view("templates/footer");
     }
+
     public function permintaanSurat(): void {
         $data['title'] = "Permintaan Surat";
         $data['active_page'] = "permintaan_surat";
+        $data['css'] = ["assets/css/sidebar"];
         $this->view("templates/header", $data);
         $this->view("pages/student/permintaan_surat", $data);
         $this->view("templates/footer");
