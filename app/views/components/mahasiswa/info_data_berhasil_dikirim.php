ffafe34c99edcce7dd1ce1cbd93a2e7bf800d8ae
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-02 10:53:10 +0700

    fix base layout for administrasi prodi page mahasiswa

---
+++ app/views/components/mahasiswa/info_data_berhasil_dikirim.php
@@ -0,0 +1,5 @@
+<div id="body-content" class="mt-4 card card-body d-flex flex-column justify-content-center align-items-center"
+    style="box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25); min-height: 75vh; max-height: 75vh;">
+    <p>Data berhasil dikirim !</p>
+    <p>Mohon tunggu verifikasi dari admin dalam 2x24 jam di hari kerja.</p>
+</div>
