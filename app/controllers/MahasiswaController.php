a1ff788e796d631458e03c1c8fe6e2f1b5c0ad53
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-02 20:16:38 +0700

    add database connectivity to insert new data from user form 'berkas ta'

--- app/controllers/MahasiswaController.php
+++ app/controllers/MahasiswaController.php
@@ -33,9 +33,9 @@ class MahasiswaController extends Controller
     {
         if (isset($_FILES['tugas_akhir']) && isset($_FILES['program_aplikasi']) && isset($_FILES['publikasi_jurnal'])) {
             $upload_dir = [
-                "tugas_akhir" => __DIR__ . "/../../storage/tugas_akhir/",
-                "program_aplikasi" => __DIR__ . "/../../storage/program_aplikasi/",
-                "publikasi_jurnal" => __DIR__ . "/../../storage/publikasi_jurnal/"
+                "tugas_akhir" => __DIR__ . "/../../storage/tugas_akhir/laporan_ta/",
+                "program_aplikasi" => __DIR__ . "/../../storage/tugas_akhir/program_aplikasi/",
+                "publikasi_jurnal" => __DIR__ . "/../../storage/tugas_akhir/publikasi_jurnal/"
             ];
 
             foreach ($upload_dir as $key => $path) {
@@ -45,15 +45,27 @@ class MahasiswaController extends Controller
             }
 
             $is_move_uploaded_file_success = true;
-            foreach ($_FILES as $key => $value) {
-                $file_path = $upload_dir[$key] . uniqid() . '--' . basename($value['name']);
+            foreach ($_FILES as $key => &$value) {
+                $value['new_name'] = uniqid() . '--' . basename($value['name']);
+                $file_path = $upload_dir[$key] . $value['new_name'];
 
                 if (!move_uploaded_file($value['tmp_name'], $file_path)) {
                     $is_move_uploaded_file_success = false;
                 }
             }
 
-            if (!$is_move_uploaded_file_success) {
+            if ($is_move_uploaded_file_success) {
+                try {
+                    $this->berkas->addNewBerkasTA($_SESSION['user_id'], $_FILES['tugas_akhir']['new_name'], $_FILES['program_aplikasi']['new_name'], $_FILES['publikasi_jurnal']['new_name']);
+
+                    $_SESSION['status']['tugas_akhir'] = "diajukan";
+                    http_response_code(200);
+                    echo "Sukses mengirimkan formulir!";
+                } catch (\Exception $e) {
+                    http_response_code(500);
+                    echo "Server error! Gagal menambahkan data ke database!";
+                }
+            } else {
                 http_response_code(500);
                 echo "Gagal memasukkan file ke server";
             }
