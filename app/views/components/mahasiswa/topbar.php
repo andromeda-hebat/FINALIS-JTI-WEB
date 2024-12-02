ceca6a1044c01b0e6e0ce4e79160eaaf59671e42
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-02 10:00:26 +0700

    add: dynamic data in topbar 'mahasiswa'

--- app/views/components/mahasiswa/topbar.php
+++ app/views/components/mahasiswa/topbar.php
@@ -2,11 +2,11 @@
 	class="navbar position-absolute top-0 end-0 pe-5 d-flex justify-content-end">
 	<div class="mx-3 text-end">
 		<div class="text-center d-flex pt-3 ">
-			<img src="assets/img/foto.jpg" alt="Profil" class="rounded-circle"
+			<img src="<?= $_SESSION['user_photo'] ?>" alt="Profil" class="rounded-circle"
 				style="width: 30px; height: 30px; background-color: #ddd;">
 			<div class="ms-2 flex-column text-start">
-				<h5 class="mb-0">Stevan Zaky</h5>
-				<p class="mt-0" style="font-size: 12px;">2341720901</p>
+				<h5 class="mb-0"><?= $_SESSION['full_name'] ?></h5>
+				<p class="mt-0" style="font-size: 12px;"><?= $_SESSION['user_id'] ?></p>
 			</div>
 		</div>
 	</div>
