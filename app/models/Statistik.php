acae2c5c32f5a0877f810301894b4e158fcd7afc
afifahnisa17 <afifahnisa041@gmail.com>
2024-12-03 13:16:21 +0700

    menambah file statistik pengajuan untuk menghitung jumlah statistik pengajuan berkas

---
+++ app/models/Statistik.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use App\Core\Model;
+
+class Statistik extends Model
+{
+    public string $id_verifikasi;
+    public string $id_berkas;
+    public string $jenis_berkas;
+    public string $status_verifikasi;
+    public string $tanggal_verifikasi;
+    public string $keterangan_verifikasi;
+
+}
+?>
