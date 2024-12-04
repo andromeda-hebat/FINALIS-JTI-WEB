306bf4115e22ec9403df045137b2ba66a1e05bc2
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-05 05:43:46 +0700

    refactor admin prodi controller to be more simple

--- app/controllers/AdminProdiController.php
+++ app/controllers/AdminProdiController.php
@@ -10,16 +10,19 @@ use App\Core\Controller;
 
 class AdminProdiController extends Controller {
     public function requestVerifikasi(): void {
-        $data['title'] = "Permintaan Verifikasi";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+        $this->view("templates/header",[
+            'title' => "Permintaan Verifikasi",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_prodi/permintaan_verifikasi");
         $this->view("templates/footer");
     }
-    public function detailsRequest(): void {
-        $data['title'] = "Detail Permintaan";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    
+    public function showDetaillReq(): void {
+        $this->view("templates/header",[
+            'title' => "Detail Permintaan",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_prodi/detail_permintaan");
         $this->view("templates/footer");
     }
