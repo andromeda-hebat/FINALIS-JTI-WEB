8f09c17f6682feccf4735086ba2e5c9139a1d6f7
Stevan Zaky <stevanzaky87@gmail.com>
2024-12-05 00:20:00 +0700

    admin_prodi

--- app/views/pages/admin_prodi/permintaan_verifikasi.php
+++ app/views/pages/admin_prodi/permintaan_verifikasi.php
@@ -41,28 +41,28 @@
                                 <td>Nama Mahasiswa</td>
                                 <td>Submit form administrasi prodi</td>
                                 <td>24 Oktober, 2024</td>
-                                <td><a href="#" class="btn btn-primary">Detail</a></td>
+                                <td><a href="/detail-permintaan" class="btn btn-primary">Detail</a></td>
                             </tr>
                             <tr>
                                 <td>2341720001</td>
                                 <td>Nama Mahasiswa</td>
                                 <td>Submit form administrasi prodi</td>
                                 <td>24 Oktober, 2024</td>
-                                <td><a href="#" class="btn btn-primary">Detail</a></td>
+                                <td><a href="/detail-permintaan" class="btn btn-primary">Detail</a></td>
                             </tr>
                             <tr>
                                 <td>2341720001</td>
                                 <td>Nama Mahasiswa</td>
                                 <td>Submit form administrasi prodi</td>
                                 <td>24 Oktober, 2024</td>
-                                <td><a href="#" class="btn btn-primary">Detail</a></td>
+                                <td><a href="/detail-permintaan" class="btn btn-primary">Detail</a></td>
                             </tr>
                             <tr>
                                 <td>2341720001</td>
                                 <td>Nama Mahasiswa</td>
                                 <td>Submit form administrasi prodi</td>
                                 <td>24 Oktober, 2024</td>
-                                <td><a href="#" class="btn btn-primary">Detail</a></td>
+                                <td><a href="/detail-permintaan" class="btn btn-primary">Detail</a></td>
                             </tr>
                         </tbody>
                     </table>
