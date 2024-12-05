b2bf0e4edec9a9d72d6a2738d409e67f68797f95
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-05 19:48:57 +0700

    fix code dev error - forgot to add execute function in admin TA and admin prodi

--- app/controllers/AdminTAController.php
+++ app/controllers/AdminTAController.php
@@ -30,7 +30,6 @@ class AdminTAController extends Controller
     public function showDetailReq(int $id_verifikasi): void
     {
         try {
-            $all_req_file = $this->berkas_repository->getAllBerkasTAReq();
             $user_file = $this->berkas_repository->getSingleBerkasTAReq($id_verifikasi);
         } catch (\PDOException $e) {
             http_response_code(500);
@@ -42,21 +41,12 @@ class AdminTAController extends Controller
             return;
         }
 
-
-        // THIS CODE IS ONLY FOR TEMPORARY PURPOSE!!!
-        foreach ($all_req_file as $key => $value) {
-            if ($value->id_verifikasi == $id_verifikasi) {
-                $actual_data = $value;
-                break;
-            }
-        }
-
         $this->view("templates/header", [
             'title' => "Detail Permintaan",
             'css' => ["assets/css/sidebar"]
         ]);
         $this->view("pages/admin_ta/detail_permintaan", [
-            'user_file' => $actual_data
+            'user_file' => $user_file
         ]);
         $this->view("templates/footer");
     }
