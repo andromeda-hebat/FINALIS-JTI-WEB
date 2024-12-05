31d2e0ae066c44c6266d729b9f56e474ba223a29
Stevan Zaky <stevanzaky87@gmail.com>
2024-12-05 10:45:48 +0700

    dashboard jurusan

--- app/views/pages/admin_jurusan/dashboard.php
+++ app/views/pages/admin_jurusan/dashboard.php
@@ -6,7 +6,7 @@
         <?php include __DIR__ . '/../../components/admin_jurusan/topbar.php' ?>
 
         <div class="halaman mx-5 ">
-            <h6>Belum ada dashboard pada divisi UI/UX</h6>
+            <h6>Dashboard</h6>
 
         </div>
     </div>
