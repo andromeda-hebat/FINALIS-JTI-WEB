ce815ebc92d9c58faa7134a8ee9adc6ac78d2d16
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-07 10:17:28 +0700

    make code cleaner for admin jurusan controller

--- app/controllers/AdminJurusanController.php
+++ app/controllers/AdminJurusanController.php
@@ -2,78 +2,93 @@
 
 namespace App\Controllers;
 
-require_once __DIR__ . '/../core/Controller.php';
-
-
 use App\Core\Controller;
 
-
-class AdminJurusanController extends Controller {
-    public function kelolaAdmin(): void {
-        $data['title'] = "Kelola Admin";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+class AdminJurusanController extends Controller
+{
+    public function kelolaAdmin(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Kelola Admin",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/kelola_admin");
         $this->view("templates/footer");
     }
 
-    public function tambahAdmin(): void {
-        $data['title'] = "Tambah Admin";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    public function tambahAdmin(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Tambah Admin",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/tambah_admin");
         $this->view("templates/footer");
     }
-    public function editAdmin(): void {
-        $data['title'] = "Edit Admin";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    public function editAdmin(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Edit Admin",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/edit_admin");
         $this->view("templates/footer");
     }
 
-    public function kelolaMhs(): void {
-        $data['title'] = "Kelola Mahasiswa";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    public function kelolaMhs(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Kelola Mahasiswa",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/kelola_mahasiswa");
         $this->view("templates/footer");
     }
 
-    public function tambahMhs(): void {
-        $data['title'] = "Tambah Mahasiswa";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    public function tambahMhs(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Tambah Mahasiswa",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/tambah_mahasiswa");
         $this->view("templates/footer");
     }
-    public function editMhs(): void {
-        $data['title'] = "Edit Mahasiswa";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    public function editMhs(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Edit Mahasiswa",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/edit_mahasiswa");
         $this->view("templates/footer");
     }
 
-    public function kelolaTemplateSurat(): void {
-        $data['title'] = "Kelola Template Surat";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    public function kelolaTemplateSurat(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Kelola Template Surat",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/kelola_surat");
         $this->view("templates/footer");
     }
 
-    public function tambahTemplateSurat(): void {
-        $data['title'] = "Tambah Template Surat";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    public function tambahTemplateSurat(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Tambah Template Surat",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/tambah_surat");
         $this->view("templates/footer");
     }
-    public function catatanAktivitas(): void {
-        $data['title'] = "Log Aktivity";
-        $data['css'] = ["assets/css/sidebar"];
-        $this->view("templates/header",$data);
+    public function catatanAktivitas(): void
+    {
+        $this->view("templates/header", [
+            'title' => "Log Aktivitas",
+            'css' => ["assets/css/sidebar"]
+        ]);
         $this->view("pages/admin_jurusan/catatan_aktivitas");
         $this->view("templates/footer");
     }
