74107633010d48a2aa7f8fb3307c364ed7ab8f98
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-08 06:28:10 +0700

    change reddit sans font cdn

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -8,15 +8,18 @@
     <link rel="icon" href="/assets/icons/finalis-jti-icon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/reddit-sans@5.1.0/index.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Reddit+Sans:ital,wght@0,200..900;1,200..900&display=swap"
+        rel="stylesheet">
 
-    <!-- Custom CSS link -->
-    <?php if(isset($data['css'])): ?>
-    <?php foreach($data['css'] as $css_link): ?>
-    <link rel="stylesheet" href="<?= $css_link . '.css' ?>">
-    <?php endforeach; ?>
+    <?php if (isset($data['css'])): ?>
+        <!-- Custom CSS link -->
+        <?php foreach ($data['css'] as $css_link): ?>
+            <link rel="stylesheet" href="<?= $css_link . '.css' ?>">
+        <?php endforeach; ?>
     <?php endif; ?>
-    
+
     <style>
         * {
             font-family: 'Reddit Sans';
