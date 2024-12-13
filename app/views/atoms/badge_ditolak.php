6c6012a0023f4c070dfb7f21aa9c4c72da415b8a
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-13 14:51:33 +0700

    add: badge status to display request status

---
+++ app/views/atoms/badge_ditolak.php
@@ -0,0 +1,8 @@
+<div style="display: flex; align-items: center; padding: .2rem; border-radius: 20px; background-color: #e74c3c; color: white; border: 2px solid #c0392b;">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="size-6" width="20" height="20" style="margin-right: 5px;">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+    Ditolak
+</div>
