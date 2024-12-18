a38e436ebdaaa180f51c49ef270226b5abe07084
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-18 22:46:25 +0700

    reformat code in tambah_surat and permintaan_verifikasi

--- app/views/pages/admin_prodi/permintaan_verifikasi.php
+++ app/views/pages/admin_prodi/permintaan_verifikasi.php
@@ -1,22 +1,13 @@
-<main class="d-flex">
-    <!-- Sidebar -->
+<div class="d-flex">
     <?php include __DIR__ . '/../../components/admin_prodi/sidebar.php' ?>
     <div class="position-top w-100" style="margin-left: 35vh;">
-
         <?php include __DIR__ . '/../../components/general/topbar.php' ?>
-
-        <div class="halaman mx-5 " style="min-height:100vh; margin-top:15vh;">
+        <main class="halaman mx-5 " style="min-height:100vh; margin-top:15vh;">
             <section class="mt-5">
-
                 <h3 class="fw-bold mb-0" style="color: var(--color-navy-blue);">Permintaan Verifikasi</h3>
-
                 <div class="mt-3 d-flex justify-content-between w-100 align-items-center">
-                    <!-- Sort -->
                     <h6 class="m-0">Seluruh Permintaan</h6>
-
                 </div>
-
-                <!-- div kotak tabel -->
                 <div class="mt-3 w-100 border rounded shadow">
                     <table class="table">
                         <thead class="table">
@@ -45,7 +36,6 @@
                     </table>
                 </div>
             </section>
-
-        </div>
+        </main>
     </div>
-</main>
+</div>
